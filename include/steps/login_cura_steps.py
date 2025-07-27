071da23a886a7a679f8768cebbbd0bed833f62ae
badrusalam11 <badrusalam760@gmail.com>
2025-07-27 20:56:53 +0700

    add objects, dynamic delay, test suite collection

--- include/steps/login_cura_steps.py
+++ include/steps/login_cura_steps.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 
 from orbs.browser_factory import BrowserFactory
 from orbs.mobile_factory import MobileFactory
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @given('the user opens the login page')
 def step_open_login(context):
@@ -15,12 +17,18 @@ def step_open_login(context):
 
 @when('the user fill username {username} and password {password}')
 def step_username(context, username, password):
-    time.sleep(5)
+    # wait for the page to load
+    WebDriverWait(context.driver, 10).until(
+        EC.presence_of_element_located(("id", "btn-make-appointment"))
+    )
     context.driver.find_element("id", "btn-make-appointment").click()
+    WebDriverWait(context.driver, 10).until(
+        EC.presence_of_element_located(("id", "txt-username"))
+    )
     context.driver.find_element("id", "txt-username").send_keys(username)
     context.driver.find_element("id", "txt-password").send_keys(password)
     context.driver.find_element("id", "btn-login").click()
 
 @then('the user should see the dashboard')
 def step_dashboard(context):
-    context.driver.save_screenshot("dashboard")
+    context.driver.save_screenshot("dashboard.png")
